44522567a861da1703aa9d6f673cb4ea16064cd4
james20902 <james20902@gmail.com>
2020-11-30 23:05:51 -0500

    started work on LedColor class

--- led_driver.py
+++ led_driver.py
@@ -7,6 +7,33 @@ import enum
 from threading import Thread
 from time import sleep
 
+class LedColor():
+    h_val, s_val, v_val = 0, 0, 0
+
+    def rgb_to_hsv(self, r, g, b):
+        rgb_values = [r/255.0, g/255.0, b/255.0]
+        conversion_constants = [((rgb_values[1] - rgb_values[2]), 360), 
+                                ((rgb_values[2] - rgb_values[0]), 120),
+                                ((rgb_values[0] - rgb_values[1]), 240)]
+
+        max_color_index = rgb_values.index(max(rgb_values)) 
+        min_color_index = rgb_values.index(min(rgb_values))
+        diff = rgb_values[max_color_index] - rgb_values[min_color_index] 
+
+        if rgb_values[max_color_index] == rgb_values[min_color_index]:
+            h_val = 0
+        else:
+            h_val = (60 * (conversion_constants[max_color_index][0] / diff) + conversion_constants[max_color_index][1]) % 360
+
+        if rgb_values[max_color_index] == 0:
+            s_val = 0
+        else:
+            s_val = (diff / rgb_values[max_color_index]) * 100
+        
+        v_val = rgb_values[max_color_index] * 100
+
+        return (round(h_val), round(s_val), round(v_val))
+
 class LedMode(enum.Enum):
     #function of brightness over time as a lambda
     STATIC = lambda brightness: brightness,
@@ -28,7 +55,7 @@ class LedDriver(Thread):
         self.G_PWM = RPi.GPIO.PWM(R_CHANNEL, 100)
         self.B_PWM = RPi.GPIO.PWM(R_CHANNEL, 100)
 
-    def set_LED_brightness(self, pwm_object, val):
+    def set_led_brightness(self, pwm_object, val):
         if val < 0:
             val = 0
         if val > 100:
@@ -40,11 +67,13 @@ class LedDriver(Thread):
 
     def __run__(self):
         while True:
-            for brightness in range(0, 101):
-                if self.current_mode is not LedMode.STATIC:
-                    self.current_mode(brightness)
+            if self.current_mode is not LedMode.STATIC:            
+                for brightness in range(0, 101):
+                    self.set_led_brightness(self.R_PWM, self.current_mode(brightness))
+                    self.set_led_brightness(self.G_PWM, self.current_mode(brightness))
+                    self.set_led_brightness(self.B_PWM, self.current_mode(brightness))
                     sleep(self.UPDATE_RATE)
-        self.set_LED_brightness(self.R_PWM, 0)
-        self.set_LED_brightness(self.G_PWM, 0)
-        self.set_LED_brightness(self.B_PWM, 0)
+        self.set_led_brightness(self.R_PWM, 0)
+        self.set_led_brightness(self.G_PWM, 0)
+        self.set_led_brightness(self.B_PWM, 0)
         RPi.GPIO.cleanup()
